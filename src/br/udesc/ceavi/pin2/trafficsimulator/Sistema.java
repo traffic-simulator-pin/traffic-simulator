e2177ff58dbe8dc1916223e6899a5d782ee443cc
jefip <jefip@NoteDoJefi>
2019-06-23 22:19:38 -0300

    Integração com o módulo 4. Inclui alteração para buscar o L&F do Sistema Operacional.

--- src/br/udesc/ceavi/pin2/trafficsimulator/Sistema.java
+++ src/br/udesc/ceavi/pin2/trafficsimulator/Sistema.java
@@ -4,19 +4,23 @@ import javax.swing.JFrame;
 
 import br.udesc.ceavi.pin2.trafficsimulator.controller.ControllerPrincipal;
 import br.udesc.ceavi.pin2.trafficsimulator.view.FramePrincipal;
-
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Sistema implements Runnable {
-	
-	public static void main(String[] args) {
-		Thread thread = new Thread(new Sistema());
-		thread.start();
-	}
 
-	@Override
-	public void run() {
-		JFrame framePrincipal = new FramePrincipal(ControllerPrincipal.getInstance());
-		framePrincipal.setVisible(true);
-	}
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Sistema());
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {}
+        JFrame framePrincipal = new FramePrincipal(ControllerPrincipal.getInstance());
+        framePrincipal.setVisible(true);
+    }
 
 }
